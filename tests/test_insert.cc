ea59f594bac43a7c78f15f3f00453dab1f147489
Guillaume Lengrand <lengra_g@epita.fr>
2014-07-06 09:35:54 +0200

    update test for insert function

--- tests/test_insert.cc
+++ tests/test_insert.cc
@@ -1,9 +1,11 @@
 #include "../src/flat_set.cc"
+#include <stdlib.h>
 
 int main()
 {
     Flat_set<int> int_set = Flat_set<int>();
-    int_set.insert(2);
+    for (int i = 0; i < 10; i++)
+        int_set.insert(rand() % 100);
     std::cout << int_set << std::endl;
 
     return 0;
